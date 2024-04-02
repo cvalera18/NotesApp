a31ef2af9a39ebd5865a3068f499b1e1c4a62ca7
cvalera18 <ing.cvalera@gmail.com>
2024-04-01 23:36:40 -0300

    strings.xml

--- app/src/main/java/com/example/notesapp/NotesApp.kt
+++ app/src/main/java/com/example/notesapp/NotesApp.kt
@@ -1,7 +1,13 @@
 package com.example.notesapp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NotesApp : Application()
+class NotesApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
